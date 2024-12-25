a1dc8507ee6a478db7604430701cdc4578dd98ec
Abdullah <abdullaharslan1473@gmail.com>
2024-12-25 18:42:11 +0300

    Initial Commit

---
+++ Blablacar/BlablacarApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlablacarApp.swift
+//  Blablacar
+//
+//  Created by abdullah on 25.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct BlablacarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
